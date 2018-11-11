eb57a96b1a105cc4249beef4e025b1f94b655b62
AlejandroChM <alejandrochanmartin1@gmail.com>
2018-11-10 19:51:37 -0500

    Add files via upload

---
+++ views/layout/default/footer.php
@@ -0,0 +1,11 @@
+		<script type="text/javascript" src="<?php
+		/**
+		* footer.php cargar footer
+		*
+		* @author Jose Alejandro Chan Martin <alejandrochanmartin1@gmail.com>
+		*/
+	 	echo APP_URL_JS.'jquery-3.3.1.min.js'; ?>"></script>
+		<script type="text/javascript" src="<?php echo APP_URL_JS.'bootstrap.min.js'; ?>"></script>	
+		<script type="text/javascript" src="views/layout/default/confirmation.js"></script>	
+	</body>
+</html>
